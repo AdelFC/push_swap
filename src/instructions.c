52c23665195ad01cdde4df21e44b20eb9c47d90f
AdelFC <adel.fodil-cherif@gmail.com>
2025-02-07 17:12:54 +0100

    little add

--- src/instructions.c
+++ src/instructions.c
@@ -6,13 +6,13 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 14:22:08 by afodil-c          #+#    #+#             */
-/*   Updated: 2025/02/07 16:59:20 by afodil-c         ###   ########.fr       */
+/*   Updated: 2025/02/07 17:12:28 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	ft_swap(t_stack **stack)
+static void ft_swap(t_stack **stack)
 {
     t_stack	*first;
     t_stack	*second;
@@ -25,16 +25,20 @@ static void	ft_swap(t_stack **stack)
     second->next = first;
     *stack = second;
 }
-
-static void	ft_print_swap(t_stack **stack, char type)
+static void	do_swap(t_stack **stack, char type)
 {
     ft_swap(stack);
     if (type == 'a')
         ft_printf("sa\n");
     else if (type == 'b')
         ft_printf("sb\n");
-    else if (type == 's')
-        ft_printf("ss\n");
+}
+
+static void	do_swap_both(t_stack **stack_a, t_stack **stack_b)
+{
+    ft_swap(stack_a);
+    ft_swap(stack_b);
+    ft_printf("ss\n");
 }
 
 static void	ft_push(t_stack **src, t_stack **dest)
@@ -48,7 +52,7 @@ static void	ft_push(t_stack **src, t_stack **dest)
     ft_stackadd_front(dest, tmp);
 }
 
-static void	ft_print_push(t_stack **stack_a, t_stack **stack_b, char type)
+static void	do_push(t_stack **stack_a, t_stack **stack_b, char type)
 {
     if (type == 'a')
     {
@@ -76,15 +80,20 @@ static void	ft_rotate(t_stack **stack)
     last->next = first;
 }
 
-static void	ft_print_rotate(t_stack **stack, char type)
+static void	do_rotate(t_stack **stack, char type)
 {
     ft_rotate(stack);
     if (type == 'a')
         ft_printf("ra\n");
     else if (type == 'b')
         ft_printf("rb\n");
-    else if (type == 'r')
-        ft_printf("rr\n");
+}
+
+static void	do_rotate_both(t_stack **stack_a, t_stack **stack_b)
+{
+    ft_rotate(stack_a);
+    ft_rotate(stack_b);
+    ft_printf("rr\n");
 }
 
 static void	ft_reverse_rotate(t_stack **stack)
@@ -105,13 +114,18 @@ static void	ft_reverse_rotate(t_stack **stack)
     *stack = last;
 }
 
-static void	ft_print_reverse_rotate(t_stack **stack, char type)
+static void	do_reverse_rotate(t_stack **stack, char type)
 {
     ft_reverse_rotate(stack);
     if (type == 'a')
         ft_printf("rra\n");
     else if (type == 'b')
         ft_printf("rrb\n");
-    else if (type == 'r')
-        ft_printf("rrr\n");
+}
+
+static void	do_reverse_rotate_both(t_stack **stack_a, t_stack **stack_b)
+{
+    ft_reverse_rotate(stack_a);
+    ft_reverse_rotate(stack_b);
+    ft_printf("rrr\n");
 }
